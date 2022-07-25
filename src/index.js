6157efaca78f398a2ce21ebaa2a89e9b5c42569a
KingPing <55996207+KingPinged@users.noreply.github.com>
2022-07-25 17:04:10 +0000

    Default structure of game

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { render } from 'react-dom'
+import React, { useRef } from 'react'
+import clamp from 'lodash-es/clamp'
+import { useSprings, animated } from 'react-spring'
+import { useGesture } from 'react-use-gesture'
+import './styles.css'
+
+
+function Viewpager() {
+
+  return props.map(({ x, display, sc }, i) => (
+    <animated.div {...bind()} key={i} style={{ display, transform: x.interpolate(x => `translate3d(${x}px,0,0)`) }}>
+      <animated.div style={{ transform: sc.interpolate(s => `scale(${s})`), backgroundImage: `url(${pages[i]})` }} />
+    </animated.div>
+  ))
+}
+
+render(<Viewpager />, document.getElementById('root'))
