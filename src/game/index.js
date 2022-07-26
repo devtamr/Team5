2e982238429e0ee3ae12cc477b8da041199e90fb
Vbone6 <109823243+Vbone6@users.noreply.github.com>
2022-07-26 11:57:09 -0500

    Update index.js

--- src/game/index.js
+++ src/game/index.js
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-import logoImg from "@/assets/logo.png";
+
 
 export class MyGame extends Phaser.Scene {
   constructor() {
@@ -7,13 +7,17 @@ export class MyGame extends Phaser.Scene {
   }
 
   preload() {
-    console.log("loaded");
-    this.load.image("logo", logoImg);
+    this.load.image('Tiles', 'assests/tiles.png)
+    this.load.tilemapTiledJSON('testPhaserMap', 'assests/testPhaserMap.json')               
   }
 
   create() {
-    const logo = this.add.image(400, 150, "logo");
-
+    this.add.image(0,0,'testPhaserMap')
+    const tileset = map.addTilesetImage('standard_tiles', 'base_tiles')
+    const map = this.make.tilemap({ key: 'tilemap' })
+    map.createStaticLayer('Background', tileset)
+    map.createStaticLayer('Ground', tileset)
+    }
     this.tweens.add({
       targets: logo,
       y: 450,
